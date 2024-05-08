8adcfe7bb1147ac552e95e7b427477178e43b0e6
alfaArghya <arghya.coding@gmail.com>
2024-05-08 18:34:33 +0530

    update the page 📰

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import Footer from "@/components/elements/Footer";
+import NavBar from "@/components/elements/NavBar";
+
 export default function Home() {
   return (
-    <>
+    <div className="h-screen flex flex-col justify-between ">
+      <NavBar />
       <h1 className="flex justify-center items-center text-2xl text-sky-200">
         Hello World
       </h1>
-    </>
+      <Footer />
+    </div>
   );
 }
