6826b1ab0f2ab5ce59feaef191a2950fcf0116ab
alfaArghya <arghya.coding@gmail.com>
2024-05-08 15:56:29 +0530

    basic app setup 🚀

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="flex justify-center items-center text-2xl text-sky-200">
+        Hello World
+      </h1>
+    </>
+  );
+}
